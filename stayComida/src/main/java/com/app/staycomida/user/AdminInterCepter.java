4f782fa1837a46ef43da513681973e6a38c16a47
shinjisub <vlll3320@gmail.com>
2020-05-03 17:08:55 +0900

    InterCepter

---
+++ stayComida/src/main/java/com/app/staycomida/user/AdminInterCepter.java
@@ -0,0 +1,7 @@
+package com.app.staycomida.user;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AdminInterCepter extends HandlerInterceptorAdapter{
+
+}
