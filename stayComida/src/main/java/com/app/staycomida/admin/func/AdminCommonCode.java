af4d62588d00c8da3773c1f995878abd80180abc
bongkyo sheen <sinbk@kai-i.com>
2020-05-12 19:59:03 +0900

    ㄱㄱ

---
+++ stayComida/src/main/java/com/app/staycomida/admin/func/AdminCommonCode.java
@@ -0,0 +1,7 @@
+package com.app.staycomida.admin.func;
+
+public class AdminCommonCode {
+	private Integer successCode = 200; // 성공 코드
+	private Integer noDataCode = 101; // 데이터 없음 코드
+	private Integer validationCode = 100; // 폼 검증 실패 코드
+}
---
+++ stayComida/src/main/java/com/app/staycomida/admin/func/AjaxResult.java
@@ -0,0 +1,9 @@
+package com.app.staycomida.admin.func;
+
+public class AjaxResult extends AdminCommonCode {
+	
+	public void setRules(String key, String msg, String type) {
+		
+	}
+	
+}
